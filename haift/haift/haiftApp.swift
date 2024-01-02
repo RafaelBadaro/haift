fbc57d57b08f8cee65914a9d946d81569372dccb
Rafael Badaro <rafabadaro.15@gmail.com>
2024-01-02 20:28:20 -0300

    Initial project creation

---
+++ haift/haift/haiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  haiftApp.swift
+//  haift
+//
+//  Created by Rafael Badaró on 02/01/24.
+//
+
+import SwiftUI
+
+@main
+struct haiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
